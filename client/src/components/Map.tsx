c0f1d204d47ede4011c12bfcfa48e2e545be0d5e
Brandon Trump <brandontrump101@gmail.com>
2023-03-24 15:59:09 +1300

    changed location & zoom of map

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -4,16 +4,16 @@ import "leaflet/dist/leaflet.css";
 const Map = () => {
   return (
     <MapContainer
-      center={[37.38605, -122.08385]}
-      zoom={17}
+      center={[-36.84853, 174.76349]}
+      zoom={16}
       scrollWheelZoom={false}
-      style={{ width: "100%", height: "calc(100vh - 306px)", zIndex: "-1" }}
+      style={{ width: "100%", height: "calc(100vh - 406px)", zIndex: "-1" }}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[37.38605, -122.08385]}>
+      <Marker position={[-36.84853, 174.76349]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
