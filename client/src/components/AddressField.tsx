dbb1d041341272927143b198ab0a1239f832db0b
Brandon Trump <brandontrump101@gmail.com>
2023-03-24 15:57:37 +1300

    added regex for user input & created a handleSubmit function to set the ip address

--- client/src/components/AddressField.tsx
+++ client/src/components/AddressField.tsx
@@ -1,20 +1,42 @@
 import styles from "../styles/AddressField.module.css";
 import arrow from "../assets/arrow.svg";
+import { useState } from "react";
+import AddressInfoCard from "./AddressInfoCard";
 
 const AddressField = () => {
+  const [ipAddress, setIpAddress] = useState("");
+  const [address, setAddress] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const ipAddressRegex =
+      /^([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])$/;
+    if (ipAddressRegex.test(ipAddress)) {
+      setAddress(ipAddress);
+    } else {
+      console.log("Invalid IP address");
+    }
+  };
+
   return (
-    <div className={styles.address_container}>
-      <form className={styles.address_form}>
-        <label>IP Address Tracker</label>
-        <div className={styles.input_field}>
-          <input
-            type="text"
-            placeholder="Search for any IP address or domain"
-          ></input>
-          <button>Search</button>
-        </div>
-      </form>
-    </div>
+    <>
+      <div className={styles.address_container}>
+        <form className={styles.address_form} onSubmit={handleSubmit}>
+          <label>IP Address Tracker</label>
+          <div className={styles.input_field}>
+            <input
+              type="text"
+              placeholder="Search for any IP address or domain"
+              onChange={(e) => setIpAddress(e.target.value)}
+            ></input>
+            <button>Search</button>
+          </div>
+        </form>
+      </div>
+      <div className={styles.address_card}>
+        <AddressInfoCard address={address} />
+      </div>
+    </>
   );
 };
 
