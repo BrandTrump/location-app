f372415b765adf2b64713576b464ec1f52f1c6af
Brandon Trump <brandontrump101@gmail.com>
2023-03-28 16:15:37 +1300

    wrapped address field and map components around the map context

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,17 +1,34 @@
 import AddressField from "../components/AddressField";
 import AddressInfoCard from "../components/AddressInfoCard";
 import Map from "../components/Map";
+import { MapContext } from "../context/MapContext";
 import styles from "../styles/pages/Home.module.css";
+import { useState } from "react";
+
+interface GeoIpData {
+  ip: string;
+  location: {
+    city: string;
+    region: string;
+    timezone: string;
+    lat: number;
+    lng: number;
+  };
+  isp: string;
+}
 
 const Home = () => {
+  const [geoIpData, setGeoIpData] = useState<GeoIpData | null>(null);
   return (
     <>
-      <div className={styles.contianer}>
-        <div className={styles.address_field}>
-          <AddressField />
+      <MapContext.Provider value={{ geoIpData, setGeoIpData }}>
+        <div className={styles.contianer}>
+          <div className={styles.address_field}>
+            <AddressField />
+          </div>
         </div>
-      </div>
-      <Map />
+        <Map />
+      </MapContext.Provider>
     </>
   );
 };
