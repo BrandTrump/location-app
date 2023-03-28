f608ed1034f39b4e018e7b7bd704984e2ebe6f48
Brandon Trump <brandontrump101@gmail.com>
2023-03-28 16:18:00 +1300

    created custom hook to update the map location & used map context to get updated location cordinates

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -1,10 +1,33 @@
 import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { useContext, useEffect } from "react";
+import { MapContext } from "../context/MapContext";
 
 const Map = () => {
+  const { geoIpData } = useContext(MapContext);
+
+  // custom hook
+  function FlyMapTo() {
+    const map = useMap();
+
+    useEffect(() => {
+      map.flyTo(
+        geoIpData?.location
+          ? [geoIpData.location.lat, geoIpData.location.lng]
+          : [-36.84853, 174.76349]
+      );
+    }, [geoIpData.location]);
+
+    return null;
+  }
+
   return (
     <MapContainer
-      center={[-36.84853, 174.76349]}
+      center={
+        geoIpData?.location
+          ? [geoIpData.location.lat, geoIpData.location.lng]
+          : [-36.84853, 174.76349]
+      }
       zoom={16}
       scrollWheelZoom={false}
       style={{ width: "100%", height: "calc(100vh - 406px)", zIndex: "-1" }}
@@ -13,11 +36,18 @@ const Map = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[-36.84853, 174.76349]}>
+      <Marker
+        position={
+          geoIpData?.location
+            ? [geoIpData.location.lat, geoIpData.location.lng]
+            : [-36.84853, 174.76349]
+        }
+      >
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
       </Marker>
+      {geoIpData?.location && <FlyMapTo />}
     </MapContainer>
   );
 };
